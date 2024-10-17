1999938baa3e77e7afc993e94617dad57e832539
Jose Emmanuel Rangel Rodriguez <je.rangelrodriguez@ugto.mx>
2024-10-16 19:51:43 -0600

    prueba

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,18 @@
+import { Router } from "express"
+import { login, logout } from '../controllers/authController.js'
+import { check } from "express-validator"
+
+const router = Router()
+
+router.post(
+  '/login',
+  [
+    check('usuario', 'El usuario es obligatorio').not().isEmpty(),
+    check('password', 'El password es obligatorio').not().isEmpty()
+  ],
+  login
+)
+
+router.post('/logout', logout)
+
+export default router
